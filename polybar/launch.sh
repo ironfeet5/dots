eaa01def1fbdd6471c7e57605d1a040c118fc3b1
Kleidi Bujari <60077115+kleidibujari@users.noreply.github.com>
2021-05-09 16:40:25 -0400

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar -q main -c $HOME/.config/polybar/config.ini &
+
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI2')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar -q vert -c $HOME/.config/polybar/config.ini &
+fi
+
